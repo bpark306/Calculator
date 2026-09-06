616a6441c81cbd24208c6827cc8ca109ed29656d
brian <bpark4684@gmail.com>
2026-09-06 09:50:33 -0400

    Created operate function

--- script.js
+++ script.js
@@ -17,3 +17,18 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
     return num1 / num2;
 }
+
+function operate(num1, num2, operator) {
+    switch (operator) {
+        case '+':
+            return add(num1, num2);
+        case '-':
+            return subtract(num1, num2);
+        case 'x':
+            return multiply(num1, num2);
+        case '/':
+            return divide(num1, num2);
+        default:
+            return "ERROR";
+    }
+}
