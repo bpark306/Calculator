3c74d2d178ba0903f8b38b0d13efde532ab2e224
brian <bpark4684@gmail.com>
2026-09-06 23:46:38 -0400

    Fixed operate methods

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ let buttons = [
     ];
 
 function operate(num1, num2, operator) {
+    console.log(`${num1} ${operator} ${num2}`);
     switch (operator) {
         case '+':
             return num1 + num2;
